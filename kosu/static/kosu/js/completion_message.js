82ee91ae5bbf64192e4655acd7608de2393fc17f
yuya <oltotlo81@gmail.com>
2025-01-27 00:03:09 +0900

    CSS戻し

--- kosu/static/kosu/js/completion_message.js
+++ kosu/static/kosu/js/completion_message.js
@@ -1,24 +1,10 @@
-$(document).ready(function() {
-    let message = $('#success-message');
-
-    // 中央にメッセージを表示する関数
-    function centerMessage() {
-        message.css({
-            'top': '50%',
-            'left': '50%',
-            'transform': 'translate(-50%, -50%)'
-        });
-    }
-
-    // メッセージを表示し位置を中央に設定
-    message.css("display", "block");
-    centerMessage();
-
-    // 画面サイズ変更やスクロール時に再設定
-    $(window).on('resize scroll', centerMessage);
+$(document).ready(function(){
+    // 送信完了メッセージを表示
+    let message = document.getElementById("success-message");
+    message.style.display = "block";
 
     // 1秒後にメッセージを非表示
     setTimeout(function() {
-        message.css("display", "none");
+        message.style.display = "none";
     }, 1000);
 });
