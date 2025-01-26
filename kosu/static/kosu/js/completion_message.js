e04749ff8bcd21eab67f4d6df42a4ed30445b57c
yuya <oltotlo81@gmail.com>
2025-01-26 23:41:47 +0900

    工数登録完了CSS中央に調整

--- kosu/static/kosu/js/completion_message.js
+++ kosu/static/kosu/js/completion_message.js
@@ -2,14 +2,24 @@ $(document).ready(function(){
     var message = $("#success-message");
     
     // メッセージを表示
-    message.show();
+    message.css('display', 'block');
     
-    // 親要素の横スクロールを無効にする
-    $("body").css("overflow-x", "hidden");
+    // メッセージが表示される時に画面中央に位置させる
+    function centerMessage() {
+        message.css({
+            top: ($(window).height() - message.outerHeight()) / 2 + $(window).scrollTop() + 'px',
+            left: ($(window).width() - message.outerWidth()) / 2 + $(window).scrollLeft() + 'px',
+        });
+    }
+
+    // 画面サイズが変更される度にセンタリング
+    $(window).on('resize scroll', centerMessage);
+    
+    // 最初の表示もセンタリング
+    centerMessage();
 
     // 1秒後にメッセージを非表示
     setTimeout(function() {
-        message.hide();
-        $("body").css("overflow-x", "auto"); // 元に戻す
+        message.css('display', 'none');
     }, 1000);
 });
