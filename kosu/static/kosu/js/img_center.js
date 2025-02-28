0e7e6dd020d25fc70fc8f8fdff55857cd6e5e54e
NOMURA YUYA / 野村 侑矢 <yuya.nomura@mail.toyota-shokki.co.jp>
2025-02-28 17:09:58 +0900

    ローディング画像修正

--- kosu/static/kosu/js/img_center.js
+++ kosu/static/kosu/js/img_center.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loadingImg = document.getElementById('loadingImg'); // 画像要素取得
+    const loadingText = document.querySelector('#loading p');
     const loadingContainer = document.getElementById('loading'); // 親要素取得
 
     // 指定したIDの要素が存在するか確認
-    if (!loadingImg || !loadingContainer) {
+    if (!loadingImg || !loadingContainer || !loadingText) {
         console.error("Target elements not found in the DOM!");
         return;
     }
@@ -14,14 +15,20 @@ document.addEventListener('DOMContentLoaded', () => {
         const displayWidth = document.documentElement.clientWidth;  // 表示画面の幅
         const imgHeight = loadingImg.offsetHeight;
         const imgWidth = loadingImg.offsetWidth;
+        const textHeight = loadingText.offsetHeight;
+        const textWidth = loadingText.offsetWidth;
 
         // 画像のサイズ基準で中心を計算
         const topPosition = (displayHeight / 2) - (imgHeight / 2);
         const leftPosition = (displayWidth / 2) - (imgWidth / 2);
+        const topPosition2 = (displayHeight / 2) - (textHeight / 2) + (imgHeight / 2) +15;
+        const leftPosition2 = (displayWidth / 2) - (textWidth / 2);
 
         // 動的にスタイルを更新
         loadingImg.style.top = `${topPosition}px`; // 上下位置
         loadingImg.style.left = `${leftPosition}px`; // 左右位置
+        loadingText.style.top = `${topPosition2}px`; // 上下位置
+        loadingText.style.left = `${leftPosition2}px`; // 左右位置
     }
 
     // ウィンドウのスクロールやリサイズに応じて位置を更新
