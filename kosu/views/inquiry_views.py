7fcaa2051c3122ed5ed6fd374560e1a1dd85be77
NOMURA YUYA / 野村 侑矢 <oltotlo80@gmail.com>
2026-03-31 17:28:29 +0900

    工数編集ページ挙動修正

--- kosu/views/inquiry_views.py
+++ kosu/views/inquiry_views.py
@@ -291,10 +291,8 @@ class InquirUpdate(APIView):
     except administrator_data.DoesNotExist:
       return Response({'status': 'error', 'message': '設定データが見つかりません。'}, status=status.HTTP_401_UNAUTHORIZED)
 
-    if int(login_no) != int(inquir_instance.employee_no2) or \
-      int(login_no) not in [int(admin_data.administrator_employee_no1), int(admin_data.administrator_employee_no2), int(admin_data.administrator_employee_no3)]:
+    if int(login_no) != int(inquir_instance.employee_no2) and int(login_no) not in [int(admin_data.administrator_employee_no1), int(admin_data.administrator_employee_no2), int(admin_data.administrator_employee_no3)]:
       return Response({'status': 'error', 'message': '権限がありません。'}, status=status.HTTP_403_FORBIDDEN)
-
 
     # データ変換
     inquir_serializer = InquirSerializer(inquir_instance)
