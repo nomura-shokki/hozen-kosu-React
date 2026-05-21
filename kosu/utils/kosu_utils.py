88b4f200fdaba9a6951ac1d270c028c208d22b88
NOMURA YUYA / 野村 侑矢 <oltotlo80@gmail.com>
2026-05-21 13:19:19 +0900

    新アプリ移行

--- kosu/utils/kosu_utils.py
+++ kosu/utils/kosu_utils.py
@@ -256,7 +256,7 @@ def judgement_check(kosu_def, work, tyoku, member_obj, over_work):
 
   # 出勤、休出時、工数合計と残業に整合性がある場合の処理
   if (work == '休日' or work == 'シフト休' or work == '年休' or work == '代休' or work == '公休' or work == '欠勤') and \
-    kosu_total == 0:
+    kosu_total == 0 and int(over_work) == 0:
     # 工数入力OK_NGをOKに切り替え
     judgement = True
 
